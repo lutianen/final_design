3f4176c02ac1d235e5910dc7e0274cf235ce106e
lutianen <tianen.xd@gmail.com>
2023-12-12 20:45:29 +0800

    Update scripts

--- script/train_vgg_cifar10_10.sh
+++ script/train_vgg_cifar10_10.sh
@@ -2,23 +2,24 @@
 
 # !!! Select right project prefix and data dir
 # ArchLinux (My PC)
-PROJECT_PREFIX=/home/tianen/doc/_XiDian/___FinalDesign/FinalDesign/final_design
-DATA_DIR=/home/tianen/doc/MachineLearningData/
+# PROJECT_PREFIX=/home/tianen/doc/_XiDian/___FinalDesign/FinalDesign/final_design
+# DATA_DIR=/home/tianen/doc/MachineLearningData/
 # Ubuntu Server
-# PROJECT_PREFIX=/home/lutianen/final_design
-# DATA_DIR=/home/tianen/data/
+PROJECT_PREFIX=/home/lutianen/final_design
+DATA_DIR=/home/lutianen/data/
 
+# 10
 python ${PROJECT_PREFIX}/train_test/train_vgg_cifar.py \
     --arch vgg_cifar \
     --cfg vgg16 \
     --data_path ${DATA_DIR} \
-    --job_dir ./experiment/cifar/vgg_1 \
+    --job_dir ${PROJECT_PREFIX}/experiment/cifar/vgg_1 \
     --pretrain_model ${PROJECT_PREFIX}/pretrain_model/vgg16_cifar10.pt \
     --lr 0.01 \
-    --lr_decay_step 50 100 \
+    --lr_decay_step 50 100 150 \
     --weight_decay 0.005  \
-    --num_epochs 120 \
+    --num_epochs 200 \
     --train_batch_size 512 \
-    --gpus 0 \
-    --pr_target 0.5 \
+    --gpus 3 \
+    --pr_target 0.9 \
     --graph_gpu
---
+++ script/train_vgg_cifar10_100.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# !!! Select right project prefix and data dir
+# ArchLinux (My PC)
+# PROJECT_PREFIX=/home/tianen/doc/_XiDian/___FinalDesign/FinalDesign/final_design
+# DATA_DIR=/home/tianen/doc/MachineLearningData/
+# Ubuntu Server
+PROJECT_PREFIX=/home/lutianen/final_design
+DATA_DIR=/home/lutianen/data/
+
+# 100
+python ${PROJECT_PREFIX}/train_test/train_vgg_cifar.py \
+    --arch vgg_cifar \
+    --cfg vgg16 \
+    --data_path ${DATA_DIR} \
+    --job_dir ./experiment/cifar/vgg_1 \
+    --pretrain_model ${PROJECT_PREFIX}/pretrain_model/vgg16_cifar10.pt \
+    --lr 0.01 \
+    --lr_decay_step 50 100 150 \
+    --weight_decay 0.005  \
+    --num_epochs 200 \
+    --train_batch_size 128 \
+    --gpus 3 \
+    --pr_target 0.99 \
+    --graph_gpu
